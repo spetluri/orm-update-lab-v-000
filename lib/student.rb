764e451b95fb6df8bf6bde4d9c05abc630df99cf
Sai Petluri <spetluri.1@gmail.com>
2021-02-27 00:24:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,100 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name,grade,id=nil)
+    
+    @name = name
+    @grade = grade
+    @id = id
+    
+  end
+  
+  def self.create_table
+    
+    sql = <<-SQL
+     CREATE TABLE IF NOT EXISTS students (
+       id integer PRIMARY KEY,
+       name text,
+       grade text
+     );
+    SQL
+    DB[:conn].execute(sql)
+    
+  end
 
+  def self.drop_table
+    
+    sql = "DROP TABLE IF EXISTS students;"
+    DB[:conn].execute(sql)
+    
+  end
+  
+  def save
+    
+    if self.id
+      update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name,grade)
+        VALUES (?,?);
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
+    end
+  
+  end
+  
+  
+  def update 
+    
+    sql = <<-SQL 
+      UPDATE students 
+      SET name = ? and grade = ? 
+      WHERE id = ?;
+    SQL
+    
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
+    
+  end
+  
+  def self.create
+    
+    student = Student.new(name,grade)
+    student.save
+    student
+    
+  end
+  
+  def self.new_from_db(row)
+    
+    student = self.new()
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+    
+  end
+  
+  def self.find_by_name(name)
+    
+    sql = <<-SQL
+    
+      SELECT * 
+      FROM students
+      WHERE name = ?;
+    
+    SQL
+    
+    DB[:conn].execute(sql,name).map do |row|
+      
+      self.new_from_db(row)
+      
+    end
+    
+  end
 
 end
