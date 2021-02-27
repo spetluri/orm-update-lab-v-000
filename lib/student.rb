3c566ea8f26a929650fb883a98cbeb5b3a3c80a5
Sai Petluri <spetluri.1@gmail.com>
2021-02-27 00:39:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
   
   def self.new_from_db(row)
     
-    student = self.new()
+    student = self.new
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
